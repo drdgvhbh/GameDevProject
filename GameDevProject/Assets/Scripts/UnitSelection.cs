fcee0ea23a62cf1efa4d5206bedfd70ef92d080b
RYANLEE-PC\RYAN LEE <drdgvhbh@gmail.com>
2017-02-13 20:26:08 -0500

    Added Selection Box Functionality

---
+++ GameDevProject/Assets/Scripts/UnitSelection.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Unit selection from an RTS perspective.
+/// <remarks>
+/// Includes the RTS box selection mechanic. 
+/// </remarks>
+/// </summary>
+public class UnitSelection : MonoBehaviour {
+	public RectTransform selectionBox;
+	private bool isSelecting;
+	private Vector3 mousePosition;
+	private Camera mainCamera;
+
+	/// <summary>
+	/// Intializes the selection box to inactivate
+	/// </summary>
+	private void Start() {
+		this.isSelecting = false;
+		this.selectionBox.gameObject.SetActive(false);
+	}
+
+	/// <summary>
+	/// Activates the selection box if the left mouse button is pressed down.
+	/// </summary>
+	private void Update() {
+		if (Input.GetMouseButtonDown(0)) {
+			this.isSelecting = true;
+			this.mousePosition = Input.mousePosition;
+			Debug.Log(this.mousePosition);
+			Debug.Log("Box" + selectionBox.localPosition);
+			this.selectionBox.gameObject.SetActive(true);
+		} else if (Input.GetMouseButtonUp(0)) {
+			this.isSelecting = false;
+			this.selectionBox.gameObject.SetActive(false);
+		}
+
+		if (this.isSelecting) {
+			this.DrawSelectionBox();
+		}
+	}
+	/// <summary>
+	/// Draws the selection box again to prevent it from wobbling
+	/// </summary>
+	private void LateUpdate() {
+		if (this.isSelecting) {
+			this.DrawSelectionBox();
+		}
+	}
+
+	/// <summary>
+	/// Draws the selection box on the screen.
+	/// </summary>
+	private void DrawSelectionBox() {
+		Vector2 hypotenuse = new Vector2(Input.mousePosition.x - this.mousePosition.x, Input.mousePosition.y - this.mousePosition.y);
+		Vector2 boxAnchor = new Vector2(this.mousePosition.x - (Screen.width / 2), this.mousePosition.y - (Screen.height / 2));
+		Vector2 sizeDelta = new Vector2(0.0f, 0.0f);
+
+		if (hypotenuse.x > 0) {
+			boxAnchor.x = boxAnchor.x + this.selectionBox.sizeDelta.x / 2;
+			sizeDelta.x = hypotenuse.x;
+		} else {
+			boxAnchor.x = boxAnchor.x - this.selectionBox.sizeDelta.x / 2;
+			sizeDelta.x = (float)System.Math.Abs(hypotenuse.x);
+		}
+
+		if (hypotenuse.y > 0) {
+			boxAnchor.y = boxAnchor.y + this.selectionBox.sizeDelta.y / 2;
+			sizeDelta.y = hypotenuse.y;
+		} else {
+			boxAnchor.y = boxAnchor.y - this.selectionBox.sizeDelta.y / 2;
+			sizeDelta.y = (float)System.Math.Abs(hypotenuse.y);
+		}
+
+		selectionBox.localPosition = boxAnchor;
+		selectionBox.sizeDelta = sizeDelta;
+	}
+}
---
+++ GameDevProject/Assets/Scripts/Utils.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Utility Class
+/// </summary>
+public static class Utils {
+	/// <summary>
+	/// A texture used specifically for the unit selection box
+	/// </summary>
+	public static Texture2D whiteTexture {
+		set {
+			if (whiteTexture == null) {
+				whiteTexture = new Texture2D(1, 1);
+				whiteTexture.SetPixel(0, 0, Color.white);
+				whiteTexture.Apply();
+			}
+		}
+		get {
+			return whiteTexture;
+		}
+	}
+}
